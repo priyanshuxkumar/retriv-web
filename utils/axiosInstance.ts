5ce36b671ca7d76d8ad6f5a94d4bbd8689370d8d
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-23 00:26:44 +0530

    add custom axios

---
+++ utils/axiosInstance.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+const AxiosInstance = axios.create({
+    baseURL: 'http://localhost:8080'
+});
+
+
+AxiosInstance.interceptors.response.use(
+    (response: AxiosResponse): AxiosResponse => {
+        return response;
+    },
+    (error : AxiosError) => {
+        return error;
+        // if (error.response && error.response.data) {
+        //     return Promise.reject(error.response.data as ApiErrorResponse);
+        // }
+        // return Promise.reject(error);
+    }
+);
+
+export default AxiosInstance;

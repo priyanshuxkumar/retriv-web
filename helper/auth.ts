ca341bd6e1aaaf424a281c39f90d0a40b2a8c489
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-23 00:25:38 +0530

    add helper function

---
+++ helper/auth.ts
@@ -0,0 +1,33 @@
+import { SigninFormSchema } from "@/types";
+import { z } from "zod";
+
+interface ValidateFormProp {
+    email: string;
+    password: string;
+    setErrors: ({}) => void;
+}
+
+const validateSigninForm = ({ email, password, setErrors } : ValidateFormProp) : boolean => {
+  try {
+    SigninFormSchema.parse({ email, password });
+    setErrors({});
+    return true;
+  } catch (validationError: unknown) {
+    if (validationError instanceof z.ZodError) {
+      const fieldErrors: {
+        email?: string;
+        password?: string;
+      } = {};
+
+      validationError.errors.forEach((err) => {
+        if (err.path[0] === "email") fieldErrors.email = err.message;
+        if (err.path[0] === "password") fieldErrors.password = err.message;
+      });
+
+      setErrors(fieldErrors);
+    }
+    return false;
+  }
+};
+
+export { validateSigninForm };
---
+++ helper/time.ts
@@ -0,0 +1,9 @@
+import { formatDistanceToNow } from "date-fns";
+
+export function timeAgo(timestamp: Date) {
+    if(timestamp) {
+        return formatDistanceToNow(new Date(timestamp), { addSuffix: true });
+    } else {
+        return null
+    }
+}

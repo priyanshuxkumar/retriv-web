e30409f46ada3635ef69b6940fe99e7344a774d2
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-26 16:10:48 +0530

    add fonts and icons

---
+++ components/fonts/fonts.ts
@@ -0,0 +1,17 @@
+import { Alegreya, Aleo, Inter } from "next/font/google";
+
+
+export const alegreya = Alegreya({
+    subsets: ["latin"],
+    weight: ["400","500","600","700", "800"]
+});
+
+export const aleo = Aleo({
+    subsets: ["latin"],
+    weight: ["400","500","600", "700", "800"]
+});
+
+export const inter = Inter({
+    subsets: ["latin"],
+    weight: ["400","500","600", "700", "800"]
+});

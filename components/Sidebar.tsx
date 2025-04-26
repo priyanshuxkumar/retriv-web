f3acd1d41bfde0e41e4fb62fe73632d50ada34a4
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-26 16:10:02 +0530

    add Sidebar and UserProfile card

---
+++ components/Sidebar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import clsx from "clsx";
+import Link from "next/link";
+import { Bot, Lock, Logs, Settings2, UserRoundSearch } from "lucide-react";
+import { usePathname } from "next/navigation";
+import UserProfile from "./UserProfile";
+import { alegreya } from "./fonts/fonts";
+
+const item = [
+  {
+    icon: <Bot />,
+    title: "Agent",
+    link: "/agent",
+  },
+  {
+    icon: <Lock />,
+    title: "API keys",
+    link: "/apikeys",
+  },
+  {
+    icon: <Logs />,
+    title: "Logs",
+    link: "/logs",
+  },
+  {
+    icon: <UserRoundSearch />,
+    title: "Query",
+    link: "/query",
+  },
+  {
+    icon: <Settings2 />,
+    title: "Settings",
+    link: "/settings",
+  },
+];
+
+export default function Sidebar() {
+  const pathname = usePathname();
+  return (
+    <>
+      <nav className="mt-2 flex-1">
+        <div className={`flex justify-between px-2 mb-8 ${alegreya.className}`}>
+          <span className="text-2xl font-semibold">Retriv</span>
+        </div>
+        <UserProfile />
+        <ul className="mt-8">
+          {item.map((item) => (
+            <li key={item.title}>
+              <Link
+                href={item.link}
+                className={clsx(
+                  "flex h-10 items-center my-2 gap-2 rounded-md px-2 text-sm hover:bg-[#F5F5F7]",
+                  pathname === item.link && "bg-[#F5F5F7] font-bold" 
+                )}
+              >
+                <div className="text-sm opacity-80">{item?.icon}</div>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+}
---
+++ components/UserProfile.tsx
@@ -0,0 +1,93 @@
+import { ChevronsUpDown, LogOutIcon, User } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+import { useRouter } from "next/navigation";
+import AxiosInstance from "@/utils/axiosInstance";
+import { toast } from "sonner";
+import { useUser } from "@/context/user.context";
+
+export default function UserProfile() {
+  const { user } = useUser();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      const response = await AxiosInstance.post("/api/v1/auth/logout",
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      if (response.data.success === true) {
+        router.push('/login')
+        toast(response.data.message);
+      }
+    } catch (err: unknown) {
+      console.error(err);
+    }
+  };
+  return (
+    <>
+      {!user ? (
+        <div className="flex justify-center items-center w-full">
+            Loading...
+        </div>
+      ) : (
+        <div className="flex justify-between items-center px-1">
+          <div className="flex gap-2 items-center">
+            <div>
+              <Avatar className="rounded-md border w-11 h-11">
+                <AvatarImage src={user?.userMetadata.avatarUrl} />
+                <AvatarFallback className="rounded-md uppercase text-xl font-semibold">
+                  {user?.userMetadata.name[0]
+                    .toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="flex flex-col">
+              <span className="text-base font-semibold truncate">{user?.userMetadata.name}</span>
+              <span className="text-sm">Free plan</span>
+            </div>
+          </div>
+          <div>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="ghost" className="cursor-pointer hover:bg-transparent">
+                  <ChevronsUpDown />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-50 py-2 px-1">
+                <div>
+                  <Button
+                    onClick={() => router.push("/profile")}
+                    variant={"ghost"}
+                    className="w-full justify-start text-base"
+                  >
+                    <User />
+                    Profile
+                  </Button>
+                </div>
+                <div>
+                  <Button
+                    variant={"ghost"}
+                    className="w-full justify-start text-base text-red-500 hover:text-red-500"
+                    onClick={handleLogout}
+                  >
+                    <LogOutIcon />
+                    Logout
+                  </Button>
+                </div>
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}

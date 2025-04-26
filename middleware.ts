da60c110bd35005b7e6e69c1f636a98e98bcbacd
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-27 00:16:36 +0530

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("acc_token")?.value;
+  const { pathname } = request.nextUrl;
+
+  if (token && (pathname.startsWith("/login") || pathname.startsWith("/signup"))) {
+    return NextResponse.redirect(new URL("/agent", request.url));
+  }
+
+  const protectedRoutes = ['/agent', '/settings', '/profile', '/apikeys', '/logs', '/query']; 
+
+  if (!token && (protectedRoutes.some(item =>pathname.startsWith(item)))) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/login",
+    "/signup",
+    "/agent/:path*",
+    "/apikeys/:path*",
+    "/settings/:path*",
+    "/profile/:path*",
+    "/logs/:path*",
+    "/query/:path*",
+  ],
+};

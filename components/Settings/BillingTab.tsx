b02155608ada1dc4eb85ee516ff6b36629d468b3
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-05-04 22:59:30 +0530

    refactor billing tab of setting route

--- components/Settings/BillingTab.tsx
+++ components/Settings/BillingTab.tsx
@@ -9,12 +9,37 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import PriceCard from "../PriceCard";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import AxiosInstance from "@/utils/axiosInstance";
+
+interface PlanFeatures {
+  title: string;
+}
+
+export interface PlanProps {
+  id: string;
+  name: string;
+  price: number;
+  features: PlanFeatures[];
+}
 
 export default function SettingsBillingTab() {
   const router = useRouter();
-  const handlePushToBillingPage = async() => {
-    router.push(`/checkout/billing`)
-  }
+  const [plans, setPlans] = useState<PlanProps[]>([]);
+
+  useEffect(() => {
+    const fecthPlans = async () => {
+      try {
+        const response = await AxiosInstance.get("/api/v1/plans");
+        if (response.data.success === true) {
+          setPlans(response.data.data);
+        }
+      } catch (err: unknown) {
+        console.error(err);
+      }
+    };
+    fecthPlans();
+  }, []);
   return (
     <>
       <Card className="border-none shadow-none bg-transparent">
@@ -26,13 +51,17 @@ export default function SettingsBillingTab() {
         </CardHeader>
         <CardContent className="space-y-4">
           <div className="flex flex-col justify-center items-center">
-            <PriceCard />
-            <Button
-              onClick={handlePushToBillingPage}
-              className="mt-4 cursor-pointer h-10 text-base"
-            >
-              Buy now
-            </Button>
+            {plans.map((item) => (
+              <div key={item.id}>
+                <PriceCard {...item} />
+                <Button
+                  onClick={() => router.push(`/checkout/billing/${item.id}`)}
+                  className="mt-4 cursor-pointer h-10 text-base"
+                >
+                  Buy now
+                </Button>
+              </div>
+            ))}
           </div>
 
           <div className="rounded-lg border p-4">
@@ -40,7 +69,7 @@ export default function SettingsBillingTab() {
               <div>
                 <p className="font-medium">Pro Plan</p>
                 <p className="text-sm text-muted-foreground">
-                  $29/month, billed monthly
+                  ₹3000/month, billed monthly
                 </p>
               </div>
               <Badge>Active</Badge>

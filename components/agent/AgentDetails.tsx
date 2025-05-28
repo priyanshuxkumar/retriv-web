3522d87b844dc42ba7c19fd530dbbc29e44e23a3
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-05-28 22:36:52 +0530

    add: Agent details(data) component

--- components/agent/AgentDetails.tsx
+++ components/agent/AgentDetails.tsx
@@ -1,30 +1,19 @@
-"use client";
+'use client';
 
-import { useRef, useState } from "react";
-import { Globe, BarChart, Bot, Hammer, Clock9 } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { alegreya } from "../fonts/fonts";
-import { timeAgo } from "@/helper/time";
-import { AgentProps } from "./AgentDashboard";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Badge } from "@/components/ui/badge";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { useRef, useState } from 'react';
+import { Globe, BarChart, Bot, Hammer, Clock9 } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { alegreya } from '../fonts/fonts';
+import { timeAgo } from '@/helper/time';
+import { AgentProps } from './AgentDashboard';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Badge } from '@/components/ui/badge';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-const code = ` <script
+const code = ` <Script
     src='https://retriv.in/agent-widget.js'
     data-agent-id='111223334445555' // Your agent id
     data-api-key='rt_1234456789' // Your API Key
@@ -34,206 +23,178 @@ const code = ` <script
 `;
 
 export function AgentDetails({ agent }: { agent: AgentProps }) {
-  const codeRef = useRef<HTMLDivElement | null>(null);
-  const [copied, setCopied] = useState(false);
+    const codeRef = useRef<HTMLDivElement | null>(null);
+    const [copied, setCopied] = useState(false);
 
-  const handleCopyCode = () => {
-    if (codeRef.current) {
-      navigator.clipboard.writeText(code).then(() => {
-        setCopied(true);
-      });
-    }
-  };
-  return (
-    <div className="space-y-6">
-      <div className="flex items-start justify-between">
-        <div>
-          <div className="flex items-center gap-3">
-            <h2
-              className={`uppercase text-3xl font-bold ${alegreya.className}`}
-            >
-              {agent.name}
-            </h2>
-            <Bot size={30} />
-            <Badge
-              variant={agent.status === "Running" ? "outline" : "secondary"}
-              className="bg-[#556B2F] text-white py-1 px-2 font-medium"
-            >
-              {agent.status}
-            </Badge>
-          </div>
-        </div>
-      </div>
-
-      <Tabs defaultValue="overview" className="w-full">
-        <TabsList className="h-12 pb-0 grid w-full grid-cols-3 bg-transparent border-none shadow-none rounded-none">
-          <TabsTrigger value="overview" className="border-b cursor-pointer">
-            Overview
-          </TabsTrigger>
-          <TabsTrigger value="training" className="border-b cursor-pointer">
-            Training Data
-          </TabsTrigger>
-          <TabsTrigger value="integration" className="border-b cursor-pointer">
-            Integration
-          </TabsTrigger>
-        </TabsList>
-
-        <TabsContent value="overview" className="space-y-4 pt-4">
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
-            <Card className="border shadow-none bg-[#EEECE8]">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-lg font-semibold">
-                  Total Queries
-                </CardTitle>
-                <BarChart className="h-4 w-4" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">{agent.totalQueries}</div>
-              </CardContent>
-            </Card>
-
-            <Card className="border shadow-none bg-[#EEECE8]">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-lg font-semibold">
-                  Last Updated
-                </CardTitle>
-                <Globe className="h-4 w-4" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">
-                  {timeAgo(agent.lastUpdated)}
-                </div>
-                <p className="text-xs text-muted-foreground">
-                  Website crawled on{" "}
-                  {new Date(agent?.lastUpdated).toLocaleDateString()}
-                </p>
-              </CardContent>
-            </Card>
-
-            <Card className="border shadow-none bg-[#EEECE8]">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-lg font-semibold">
-                  Avg. Response Time
-                </CardTitle>
-                <Clock9 className="h-4 w-4" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">
-                  {agent.avgResponseTime}
-                </div>
-              </CardContent>
-            </Card>
-
-            <Card className="border shadow-none bg-[#EEECE8]">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-lg font-semibold">
-                  This Month Queries
-                </CardTitle>
-                <BarChart className="h-4 w-4" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">
-                  {agent.thisMonthQueries}
-                </div>
-              </CardContent>
-            </Card>
-          </div>
-        </TabsContent>
-
-        <TabsContent value="training" className="pt-4">
-          <Card className="border shadow-none bg-[#EEECE8]">
-            <CardHeader>
-              <div className="flex items-center gap-2">
-                <Hammer className="h-5 w-5" />
-                <CardTitle className="text-xl">Training Data</CardTitle>
-              </div>
-              <CardDescription>
-                Sources your agent uses to answer questions
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className="space-y-4">
+    const handleCopyCode = () => {
+        if (codeRef.current) {
+            navigator.clipboard.writeText(code).then(() => {
+                setCopied(true);
+            });
+        }
+    };
+    return (
+        <div className="space-y-6">
+            <div className="flex items-start justify-between">
                 <div>
-                  <div className="flex items-center gap-2">
-                    <Globe className="h-5 w-5" />
-                    <div className="font-semibold text-xl">Website</div>
-                  </div>
-                  <div className="mb-3 mt-2">
-                    <div className="flex items-center justify-between">
-                      <div>
-                        <p className="font-medium">{agent.sourceUrl}</p>
-                        <p className="text-sm text-muted-foreground">
-                          {agent.metadata.sourceSubUrls.length} pages crawled
-                        </p>
-                      </div>
-                      <TooltipProvider>
-                        <Tooltip>
-                          <TooltipTrigger asChild>
-                            <Button
-                              variant="outline"
-                              className="rounded-full bg-white hover:bg-white border-none cursor-pointer shadow-none"
-                              size="sm"
-                              disabled
-                            >
-                              Recrawl
-                            </Button>
-                          </TooltipTrigger>
-                          <TooltipContent>
-                            <p>Coming soon</p>
-                          </TooltipContent>
-                        </Tooltip>
-                      </TooltipProvider>
+                    <div className="flex items-center gap-3">
+                        <h2 className={`uppercase text-3xl font-bold ${alegreya.className}`}>{agent.name}</h2>
+                        <Bot size={30} />
+                        <Badge
+                            variant={agent.status === 'Running' ? 'outline' : 'secondary'}
+                            className="bg-[#556B2F] text-white py-1 px-2 font-medium"
+                        >
+                            {agent.status}
+                        </Badge>
                     </div>
-                  </div>
                 </div>
-              </div>
-            </CardContent>
-          </Card>
-        </TabsContent>
+            </div>
 
-        <TabsContent value="integration" className="pt-4">
-          <Card className="bg-[#EEECE8]">
-            <CardHeader>
-              <CardTitle>Website Integration</CardTitle>
-              <CardDescription>Add your agent to your website</CardDescription>
-            </CardHeader>
-            <CardContent className="bg-transparent">
-              <div className="space-y-4 bg-transparent">
-                <div className="rounded-md pt-4">
-                  <h3 className="font-medium mb-2">
-                    Add this script to your website
-                  </h3>
-                  <div
-                    ref={codeRef}
-                    className="bg-background rounded-md p-4 font-mono text-sm overflow-x-auto"
-                  >
-                    <SyntaxHighlighter
-                      language="javascript"
-                      style={docco}
-                      customStyle={{
-                        padding: "12px",
-                        fontSize: "15px",
-                        borderRadius: "8px",
-                      }}
-                      wrapLongLines={true}
-                    >
-                      {code}
-                    </SyntaxHighlighter>
-                  </div>
-                  <Button
-                    onClick={handleCopyCode}
-                    variant="outline"
-                    size="sm"
-                    className="mt-2"
-                  >
-                    {copied ? "Copied!" : "Copy"}
-                  </Button>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-        </TabsContent>
-      </Tabs>
-    </div>
-  );
+            <Tabs defaultValue="overview" className="w-full">
+                <TabsList className="h-12 pb-0 grid w-full grid-cols-3 bg-transparent border-none shadow-none rounded-none">
+                    <TabsTrigger value="overview" className="border-b cursor-pointer">
+                        Overview
+                    </TabsTrigger>
+                    <TabsTrigger value="training" className="border-b cursor-pointer">
+                        Training Data
+                    </TabsTrigger>
+                    <TabsTrigger value="integration" className="border-b cursor-pointer">
+                        Integration
+                    </TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="overview" className="space-y-4 pt-4">
+                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
+                        <Card className="border shadow-none bg-[#EEECE8]">
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-lg font-semibold">Total Queries</CardTitle>
+                                <BarChart className="h-4 w-4" />
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{agent.totalQueries}</div>
+                            </CardContent>
+                        </Card>
+
+                        <Card className="border shadow-none bg-[#EEECE8]">
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-lg font-semibold">Last Updated</CardTitle>
+                                <Globe className="h-4 w-4" />
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{timeAgo(agent.lastUpdated)}</div>
+                                <p className="text-xs text-muted-foreground">
+                                    Website crawled on {new Date(agent?.lastUpdated).toLocaleDateString()}
+                                </p>
+                            </CardContent>
+                        </Card>
+
+                        <Card className="border shadow-none bg-[#EEECE8]">
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-lg font-semibold">Avg. Response Time</CardTitle>
+                                <Clock9 className="h-4 w-4" />
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{agent.avgResponseTime}</div>
+                            </CardContent>
+                        </Card>
+
+                        <Card className="border shadow-none bg-[#EEECE8]">
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-lg font-semibold">This Month Queries</CardTitle>
+                                <BarChart className="h-4 w-4" />
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{agent.thisMonthQueries}</div>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </TabsContent>
+
+                <TabsContent value="training" className="pt-4">
+                    <Card className="border shadow-none bg-[#EEECE8]">
+                        <CardHeader>
+                            <div className="flex items-center gap-2">
+                                <Hammer className="h-5 w-5" />
+                                <CardTitle className="text-xl">Training Data</CardTitle>
+                            </div>
+                            <CardDescription>Sources your agent uses to answer questions</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="space-y-4">
+                                <div>
+                                    <div className="flex items-center gap-2">
+                                        <Globe className="h-5 w-5" />
+                                        <div className="font-semibold text-xl">Website</div>
+                                    </div>
+                                    <div className="mb-3 mt-2">
+                                        <div className="flex items-center justify-between">
+                                            <div>
+                                                <p className="font-medium">{agent.sourceUrl}</p>
+                                                <p className="text-sm text-muted-foreground">
+                                                    {agent.metadata.sourceSubUrls.length} pages crawled
+                                                </p>
+                                            </div>
+                                            <TooltipProvider>
+                                                <Tooltip>
+                                                    <TooltipTrigger asChild>
+                                                        <Button
+                                                            variant="outline"
+                                                            className="rounded-full bg-white hover:bg-white border-none cursor-pointer shadow-none"
+                                                            size="sm"
+                                                            disabled
+                                                        >
+                                                            Recrawl
+                                                        </Button>
+                                                    </TooltipTrigger>
+                                                    <TooltipContent>
+                                                        <p>Coming soon</p>
+                                                    </TooltipContent>
+                                                </Tooltip>
+                                            </TooltipProvider>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+
+                <TabsContent value="integration" className="pt-4">
+                    <Card className="bg-[#EEECE8]">
+                        <CardHeader>
+                            <CardTitle>Website Integration</CardTitle>
+                            <CardDescription>Add your agent to your website</CardDescription>
+                        </CardHeader>
+                        <CardContent className="bg-transparent">
+                            <div className="space-y-4 bg-transparent">
+                                <div className="rounded-md pt-4">
+                                    <h3 className="font-medium mb-2">Add this script to your website</h3>
+                                    <div
+                                        ref={codeRef}
+                                        className="bg-background rounded-md p-4 font-mono text-sm overflow-x-auto"
+                                    >
+                                        <SyntaxHighlighter
+                                            language="javascript"
+                                            style={docco}
+                                            customStyle={{
+                                                padding: '12px',
+                                                fontSize: '15px',
+                                                borderRadius: '8px',
+                                            }}
+                                            wrapLongLines={true}
+                                        >
+                                            {code}
+                                        </SyntaxHighlighter>
+                                    </div>
+                                    <Button onClick={handleCopyCode} variant="outline" size="sm" className="mt-2">
+                                        {copied ? 'Copied!' : 'Copy'}
+                                    </Button>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+            </Tabs>
+        </div>
+    );
 }

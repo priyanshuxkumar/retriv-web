f704784ae108c6eb33058878c2540b27ea5b3c94
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-06-03 23:21:36 +0530

    add: Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,9 @@
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
 import { alegreya } from './fonts/fonts';
 
 export function Header() {
     return (
-        <header className="fixed top-0 left-0 right-0 z-50">
+        <header className="sticky top-0 left-0 right-0 z-50">
             <div className="flex items-center justify-between px-6 py-4 bg-[#F9F6F0] dark:bg-black border-b">
                 <div className="flex items-center gap-2">
                     <Link href="/" className={`flex items-center gap-3 ${alegreya.className}`}>
@@ -18,12 +17,10 @@ export function Header() {
                     <Link href="#pricing" className="text-base transition-colors">
                         Pricing
                     </Link>
+                    <Link href="/legal/terms-of-services" className="text-base transition-colors">
+                        Terms
+                    </Link>
                 </nav>
-                <Link href={'/signup'}>
-                    <Button variant="secondary" className="bg-[#556B2F] text-white hover:bg-[#4A5F25]">
-                        Get started
-                    </Button>
-                </Link>
             </div>
         </header>
     );

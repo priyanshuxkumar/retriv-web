a6a37d9695236e2605f5e924e2b88475e6d2b069
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-06-19 02:32:50 +0530

    feat: add theme detection from localStorage to Loader component

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,6 +2,7 @@
 
 import { LoaderCircle } from 'lucide-react';
 import { getLocalStorage } from '@/lib/storage';
+import { useEffect, useState } from 'react';
 
 interface LoaderProp {
     strokeWidth: string;
@@ -9,10 +10,18 @@ interface LoaderProp {
 }
 
 export default function Loader({ strokeWidth = '2px', size = '1.5px' }: LoaderProp) {
-    const isCurrentThemeDark = getLocalStorage('isDarkTheme');
+    const [isDarkTheme, setIsDarkTheme] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        const isCurrentThemeDark = getLocalStorage('isDarkTheme');
+
+        setIsDarkTheme(isCurrentThemeDark === 'true');
+    }, []);
+
+    if (isDarkTheme === null) return null;
     return (
         <div className="animate-spin">
-            <LoaderCircle color={isCurrentThemeDark ? 'white' : 'black'} strokeWidth={strokeWidth} size={size} />
+            <LoaderCircle color={isDarkTheme ? 'white' : 'black'} strokeWidth={strokeWidth} size={size} />
         </div>
     );
 }

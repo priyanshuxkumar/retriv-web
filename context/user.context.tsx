d25d3d26bd5db7797b32248e9b8c5a1e83833917
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-06-19 02:36:37 +0530

    feat: add error handling and user details on localstorage

--- context/user.context.tsx
+++ context/user.context.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { getLocalStorage, setLocalStorage } from '@/lib/storage';
 import AxiosInstance from '@/utils/axiosInstance';
+import { AxiosError } from 'axios';
 import { createContext, useContext, useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
 export interface User {
     id: number;
@@ -37,6 +40,7 @@ export const useUser = () => {
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
     const [user, setUser] = useState<User | null>(null);
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
     useEffect(() => {
         (async () => {
             try {
@@ -47,13 +51,39 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
                     },
                 });
                 if (response.data.success == true) {
+                    const userData = response.data.data;
                     setUser(response.data.data);
                     setIsAuthenticated(true);
+
+                    // Set data to local storage
+                    const isNameAlreadyExist = getLocalStorage('user_fullname');
+                    if (!isNameAlreadyExist && userData?.userMetadata.name) {
+                        setLocalStorage('user_fullname', userData.userMetadata.name);
+                    }
+
+                    const isEmailAlreadyExist = getLocalStorage('user_email');
+                    if (!isEmailAlreadyExist && userData?.userMetadata.email) {
+                        setLocalStorage('user_email', userData.userMetadata.email);
+                    }
                 }
             } catch (err) {
-                console.error(err);
+                const error = err as AxiosError;
+
+                if (error.response) {
+                    toast.error('Failed to fetch logged in user details', {
+                        description: (error.response.data as AxiosError)?.message || 'An error occurred',
+                    });
+                } else if (error.request) {
+                    toast.error('Network error', {
+                        description: 'No response from server. Please check your connection.',
+                    });
+                } else {
+                    toast.error('Unexpected error', {
+                        description: error.message,
+                    });
+                }
             }
         })();
-    }, []);
+    }, [user?.userMetadata.email, user?.userMetadata.name]);
     return <UserContext.Provider value={{ user, isAuthenticated }}>{children}</UserContext.Provider>;
 };

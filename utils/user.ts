14c896b986d523b3bac237677d80fad3ed052f28
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-06-20 16:24:58 +0530

    feat: clear local storage after logout

--- utils/user.ts
+++ utils/user.ts
@@ -1,6 +1,7 @@
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { toast } from 'sonner';
 import AxiosInstance from './axiosInstance';
+import { resetLocalStorage } from '@/lib/storage';
 
 const handleLogout = async (router: AppRouterInstance) => {
     try {
@@ -13,6 +14,9 @@ const handleLogout = async (router: AppRouterInstance) => {
         );
         if (response.data.success === true) {
             router.push('/login');
+            resetLocalStorage('user_fullname');
+            resetLocalStorage('user_email');
+
             toast(response.data.message);
         }
     } catch (err: unknown) {

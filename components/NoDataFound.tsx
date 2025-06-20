778f7ffe6b599eeef63a3f087f029e3100612a8a
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-06-20 16:13:04 +0530

    feat: add content prop to NoDataFound component

--- components/NoDataFound.tsx
+++ components/NoDataFound.tsx
@@ -1,7 +1,15 @@
-export default function NoDataFound() {
+interface NoDataFoundProp {
+    content?: string;
+}
+
+const NoDataFound: React.FC<NoDataFoundProp> = ({ content }) => {
     return (
-        <div className="mt-4 flex h-80 flex-col items-center justify-center gap-8 rounded-lg border border-slate-6 p-6">
-            <div className="flex max-w-md flex-col gap-2 text-center font-semibold text-2xl">No result found</div>
+        <div className="m-4 mt-4 flex h-80 flex-col items-center justify-center gap-8 rounded-lg border border-slate-6 p-6">
+            <div className="flex max-w-md flex-col gap-2 text-center font-semibold text-2xl">
+                {content ? content : 'No result found'}
+            </div>
         </div>
     );
-}
+};
+
+export default NoDataFound;
